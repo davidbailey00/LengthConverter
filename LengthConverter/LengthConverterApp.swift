45b5cd8c31b63ba4a1c7e667ba9657046612c4b2
David Bailey <davidbailey00@outlook.com>
2021-05-15 10:39:28 +0100

    Initial Commit

---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by David Bailey on 15/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
