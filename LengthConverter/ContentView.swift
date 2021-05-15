879e2f814d0a4f4e6b4503bee68e14544f50b990
David Bailey <davidbailey00@outlook.com>
2021-05-15 11:18:31 +0100

    Create input length form section

--- LengthConverter/ContentView.swift
+++ LengthConverter/ContentView.swift
@@ -7,10 +7,38 @@
 
 import SwiftUI
 
+enum Measurement: String, CaseIterable, Identifiable {
+    case meters
+    case kilometers
+    case feet
+    case yards
+    case miles
+
+    var id: String { self.rawValue }
+}
+
 struct ContentView: View {
+    @State private var inputLength = ""
+    @State private var inputMeasurement = Measurement.meters
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Original Length")) {
+                    HStack {
+                        TextField("Length", text: $inputLength)
+                            .keyboardType(.decimalPad)
+                        Text(inputMeasurement.rawValue)
+                    }
+                    Picker("Unit", selection: $inputMeasurement) {
+                        ForEach(Measurement.allCases, id: \.self) {
+                            Text($0.rawValue)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("Length Converter")
+        }
     }
 }
 
